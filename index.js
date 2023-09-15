c43629d8bf48567a118e13c6e374401dd43a41a5
Adolf <adolfnyerere@gmail.com>
2023-09-16 02:15:14 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,40 @@
+function add (){
+
+}
+function subtract (){
+
+}
+function multiply (){
+
+}
+function divide (){
+
+}
+function add (a, b){
+    return (a + b);
+}
+function subtract (a, b){
+    return (a - b);
+}
+function multiply (a, b){
+    return (a * b);
+}
+function divide (a, b){
+    return (a / b);
+}
+function increment (a, b){
+    return (a + 1);
+}
+function decrement (a, b){
+    return (a - 1);
+}
+function makeInt(a) {
+    return (a);
+}
+function makeInt(a) {
+    return (parseFloat(a, 10));
+}
+
+function preserveDecimal(a) {
+    return (parseFloat(a));
+}
